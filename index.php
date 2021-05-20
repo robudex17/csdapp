9a2a65761a47e6f303ad43f751a4af6e857e701f
root <root@asterisk1.sbtphilippines.inc>
2021-05-21 00:49:42 +0800

    commited

--- index.php
+++ index.php
@@ -28,7 +28,7 @@
  <body class="bg-light" onload="getLoginUser()">
 
     <nav class="navbar navbar-expand-lg fixed-top navbar-dark bg-dark">
-      <a class="navbar-brand mr-auto mr-lg-0 " href="#">CSD PHILIPPINES CALLS MONITORING</a><span><button  class="btn btn-primary btn-small btn-nav"><i class="fas fa-blender-phone"></i></button></span>
+      <a class="navbar-brand mr-auto mr-lg-0 " href="#">CSD PHILIPPINES CALLS MONITORING</a><span style="margin:3px"><button  class="btn btn-primary btn-small btn-nav" ><i class="fa fa-phone" aria-hidden="true"  style="font-size:15px"></i></button></span>
        <div class="navbar-collapse collapse w-100 order-3 dual-collapse2">
         <ul class="navbar-nav ml-auto">
             <li class="nav-item">
