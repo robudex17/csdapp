6d68d6ab0e3e6a0c1621704ea87cbb11644a24e0
Rogmer Bulaclac <rogmer.bulaclac@gmail>
2021-05-21 00:27:31 +0800

    commit 2021-1

--- index.php
+++ index.php
@@ -12,7 +12,7 @@
 
     <!-- Bootstrap core CSS -->
     <link href="css/bootstrap.min.css" rel="stylesheet">
-
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
     <!-- Custom styles for this template -->
     <link href="css/offcanvas.css" rel="stylesheet">
   </head>
