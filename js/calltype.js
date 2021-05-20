0d032cc27675c874d897ed9b19c1ee289287cad6
Rogmer Bulaclac <rogmer.bulaclac@gmail>
2021-05-21 02:23:56 +0800

    commited May 21 2021

--- js/calltype.js
+++ js/calltype.js
@@ -4,7 +4,7 @@ var calltype = document.getElementById('calltype');
 var blended = document.getElementById('blended').value;
 
 alert(blended);
-if(blended === 1 ){
+if(blended === "1" ){
     calltype.style.display = "block";
 }else{
     calltype.style.display = "none";
