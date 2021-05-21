4abd62bcbf510bde155ebaa02911d629265f9c6c
Rogmer Bulaclac <rogmer.bulaclac@gmail>
2021-05-21 19:54:52 +0800

    commited May 21 2021

---
+++ api/getCalltype.php
@@ -0,0 +1,37 @@
+<?php
+//required headers
+header("Access-Control-Allow-Origin: * ");
+header("Content-Type: application/json; charset=UTF-8");
+date_default_timezone_set('Asia/Tokyo');
+
+// // database connection will be here...
+
+// //include database and object files
+include_once '../config/database.php';
+include_once '../objects/csd.php';
+
+$database = new Database();
+$db = $database->getConnection();
+
+$csd = new Csd($db);
+
+if(isset($_GET['extension']) ) {
+	$extension = $_GET['extension'];
+	$stmnt = $csd->getCallType($extension);
+	$num = $stmnt->rowCount();
+	$agents_logs = array();
+
+	if($num != 0){
+        $row = $stmnt->fetch(PDO::FETCH_ASSOC);
+        $calltype = $row['calltype'];
+
+		echo $calltype;
+
+	}else{
+		echo json_encode(array("message" => "No Details"));
+	}
+
+
+}else{
+	echo json_encode(array("message" => "No Details"));
+}
