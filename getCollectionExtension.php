055bcdd463472e3b1ed0ad2bc683bd4539be0e24
Rogmer Bulaclac <rogmer.bulaclac@gmail>
2021-05-25 00:54:50 +0800

    commited May 21 2021

--- getCollectionExtension.php
+++ getCollectionExtension.php
@@ -55,11 +55,11 @@ foreach ($servers as $server){
    $retval=null;
    $rsync=null;
   if($server === "61.194.115.115"){
-     $rsync = 'rsync -a -e "ssh -p 20022" collection_extension_list.txt root@'".$server."':/root/SCRIPTS';
+     $rsync = 'rsync -a -e "ssh -p 20022" collection_extension_list.txt root@'.$server.':/root/SCRIPTS';
      
   }else{
 
-  } $rsync = 'rsync -a -e "ssh  collection_extension_list.txt root@'.$server.':/root/SCRIPTS';
+  } $rsync = 'rsync -a -e   collection_extension_list.txt root@'.$server.':/root/SCRIPTS';
 
    exec($rsync, $output, $retval);
    if ($retval != 0){
