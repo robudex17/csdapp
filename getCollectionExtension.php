c016e78a933dc56a10c674ca57d730440813d642
Rogmer Bulaclac <rogmer.bulaclac@gmail>
2021-05-25 00:50:33 +0800

    commited May 21 2021

--- getCollectionExtension.php
+++ getCollectionExtension.php
@@ -42,36 +42,36 @@ fwrite($fp, $collection_string_pattern);
 fclose($fp);
 
 
-$servers = array("61.194.115.115", "211.0.1.128.97", "211.0.1.128.98", "211.0.1.128.100","211.0.1.128.101");
+// $servers = array("61.194.115.115", "211.0.1.128.97", "211.0.1.128.98", "211.0.1.128.100","211.0.1.128.101");
 
-exec('rsync -a -e "ssh -p 20022" collection_extension_list.txt root@61.194.115.115:/root/SCRIPTS', $output, $retval);
- if($retval != 0){
-    echo "Transfer was not successfull";
- }else{
-   echo json_encode(array("message" => "Successfully Updated CallType"));
-}
-// foreach ($servers as $server){
-//    $output=null;
-//    $retval=null;
-//    $rsync=null;
-//   if($server === "61.194.115.115"){
-//      $rsync = 'rsync -a -e "ssh -p 20022" collection_extension_list.txt root@'.$server.':/root/SCRIPTS';
+// exec('rsync -a -e "ssh -p 20022" collection_extension_list.txt root@61.194.115.115:/root/SCRIPTS', $output, $retval);
+//  if($retval != 0){
+//     echo "Transfer was not successfull";
+//  }else{
+//    echo json_encode(array("message" => "Successfully Updated CallType"));
+// }
+foreach ($servers as $server){
+   $output=null;
+   $retval=null;
+   $rsync=null;
+  if($server === "61.194.115.115"){
+     $rsync = 'rsync -a -e "ssh -p 20022" collection_extension_list.txt root@'.$server.':/root/SCRIPTS';
      
-//   }else{
+  }else{
 
-//   } $rsync = 'rsync -a -e "ssh  collection_extension_list.txt root@'.$server.':/root/SCRIPTS';
+  } $rsync = 'rsync -a -e "ssh  collection_extension_list.txt root@'.$server.':/root/SCRIPTS';
 
-//    exec($rsync, $output, $retval);
-//    if ($retval != 0){
-//       $logdate = date("Y-m-d");
-//       $serverlog = "rsync is not successfull on server" . $server . "date" . $logdate;
-//       $logsyncfile = fopen('rsync.log', 'a');
-//       fwrite($logsyncfile, $serverlog);
-//       fclose($logsyncfile);
-//    }else{
-//       echo json_encode(array("message" => "Successfully Updated CallType"));
-//    }
-//  }
+   exec($rsync, $output, $retval);
+   if ($retval != 0){
+      $logdate = date("Y-m-d");
+      $serverlog = "rsync is not successfull on server" . $server . "date" . $logdate;
+      $logsyncfile = fopen('rsync.log', 'a');
+      fwrite($logsyncfile, $serverlog);
+      fclose($logsyncfile);
+   }else{
+      echo json_encode(array("message" => "Successfully Updated CallType"));
+   }
+ }
 
 
 
